c725f5c274c2e6b822356521994b9b505cdaadb6
SuperWD40 <matteo.bernard@outlook.fr>
2025-04-08 10:21:18 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 from setuptools import setup, find_packages
-import os
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-  name = 'finance_toolkit',
-  packages = find_packages(),
-  version = '0.0.1',
-  license= 'MIT',
-  description = 'Fonction financières basiques',
-  author = 'Mattéo Bernard',
-  author_email = 'matteo.bernard@outlook.fr',
-  url = 'https://github.com/Matteo-Bernard',
-  download_url = 'https://github.com/Matteo-Bernard/finance_toolkit/archive/refs/tags/v0.1.tar.gz',
-  keywords = ['SCRAPING', 'FINANCE'],
-  install_requires = ['pandas', 'numpy', 'typing'],
-  long_description = open('README.md').read(),
-  long_description_content_type='text/markdown',
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Scraping Tools',
-    'License :: OSI Approved :: MIT License',
-  ],
+    name="finance_toolkit",
+    version="0.0.1",
+    description="Toolkit financier",
+    author="Matteo Bernard",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "typing"
+    ],
+    include_package_data=True,
 )
