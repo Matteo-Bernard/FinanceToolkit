32dafc6825a33f04bc73dded15e880e4a53ee961
SuperWD40 <matteo.bernard@outlook.fr>
2025-04-08 10:14:21 +0200

    add init

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+
+from .finance import (
+    beta,
+    theta,
+    sigma,
+    max_drawdown,
+    jensen_alpha,
+    alpha,
+    sharpe_ratio,
+    calmar_ratio,
+    indexing,
+    historical_var,
+    momentum
+)
