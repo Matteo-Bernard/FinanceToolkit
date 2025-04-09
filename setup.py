27275dd3606aa3857c82ea46754afedf926deecd
SuperWD40 <matteo.bernard@outlook.fr>
2025-04-09 10:04:39 +0200

    Packaging 3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="FinanceKit",
+    name="FinanceToolkit",
     version="0.0.1",
     description="Toolkit financier",
     author="Matteo Bernard",
